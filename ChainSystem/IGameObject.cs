b0877216758cdd3d4bab649b3a0975e322324d83
convert314 <convert314@gmail.com>
2015-06-14 02:19:47 +0900

    IGanemObjectの内容を記述。

--- ChainSystem/IGameObject.cs
+++ ChainSystem/IGameObject.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace ChainSystem
 {
-    public interface IGameObject
+    public interface IGameObject : IObservable<News>
     {
+        String GetName(Int64 securityClearance);
+        String Name { get; }
+        String GetDescritption(Int64 securityClearance);
+        Boolean HasAttribute(String name, Int64 securityClearance);
+        T GetAttribute<T>(String name, Int64 securityClearance);
     }
 }
