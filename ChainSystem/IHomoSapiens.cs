de53465396f6e45e0528e17b7d13f03f8d1dc588
convert314 <convert314@gmail.com>
2015-06-14 02:57:50 +0900

    新クラス追加。 IHomoSapiensに内容追加。

--- ChainSystem/IHomoSapiens.cs
+++ ChainSystem/IHomoSapiens.cs
@@ -8,5 +8,23 @@ namespace ChainSystem
 {
     public interface IHomoSapiens
     {
+        Int16 MaxHitPoint { get; set; }
+        Int16 HitPoint { get; set; }
+        Int16 MaxMagicPoint { get; set; }
+        Int16 MagicPoint { get; set; }
+        Int16 MaxSanityPoint { get; set; }
+        Int16 SanityPoint { get; set; }
+
+        Int16 Strength { get; set; }
+        Int16 Intelligence { get; set; }
+        Int16 Constitution { get; set; }
+        Int16 Dexterity { get; set; }
+        Int16 Appearance { get; set; }
+        Int16 Education { get; set; }
+        Int16 Size { get; set; }
+
+        Int16 Luck { get; set; }
+        Int16 Idea { get; set; }
+        Int16 Knowledge { get; set; }
     }
 }
---
+++ ChainSystem/SexualTendency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainSystem
+{
+    public sealed class SexualTendency
+    {
+        Int16 MaxHomo { get; set; }
+        Int16 MinHomo { get; set; }
+        Int16 Homo { get; set; }
+        Int16 MaxHetero { get; set; }
+        Int16 MinHetero { get; set; }
+        Int16 Hetero { get; set; }
+
+        public SexualTendency() { }
+        public SexualTendency(Random random) {
+        
+        }
+    }
+}
