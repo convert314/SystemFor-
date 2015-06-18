2d0babe63f779d08e5169764d791b80bc7dcc50e
convert314 <convert314@gmail.com>
2015-06-18 12:24:40 +0900

    DiceにParseメソッド追加

--- ChainSystem/Dice.cs
+++ ChainSystem/Dice.cs
@@ -47,6 +47,107 @@ namespace ChainSystem
             return ranges.All(range => range > 0 && range <= 100);
         }
 
+        /// <summary>
+        /// !xdy形式あるいは!xDy形式で記述されたダイスの文字列表現を解析し、Dice型のインスタンスを返します
+        /// </summary>
+        /// <param name="s">ダイスの文字列表現</param>
+        /// <returns>Diceのインスタンス</returns>
+        public static Dice Parse(String s)
+        {
+            return _Parse_GetAnswer(_Parse_CutIntoPieces(s.Trim(new Char[] { ' ', '　', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200a', '\u200b' }).Where(c => !new Char[] { ' ', '　', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200a', '\u200b' }.Contains(c)).ToArray()));
+        }
+
+        /// <summary>
+        /// Parseメソッド下でのみ使用される
+        /// +!xDy、-!xDy、+xxx、-xxxに分割されたListを受け取り、Diceを返す
+        /// </summary>
+        /// <param name="splited">要素が単一ダイスあるいは定数として正規化されているChar[]のリスト</param>
+        /// <returns></returns>
+        private static Dice _Parse_GetAnswer(List<Char[]> splited)
+        {
+            var answer = new Dice();
+            foreach (var chars in splited)
+            {
+                if (chars == null || chars.Length == 0) continue;
+                else if (chars[0] == '+')
+                {
+                    if (Char.IsNumber(chars[1]))
+                    {
+                        answer += Int64.Parse(new String(chars));
+                    }
+                    else if (chars[1] == '!')
+                    {
+                        var count = Int64.Parse(new String(chars.Skip(2).TakeWhile(c => Char.ToLower(c) != 'd').ToArray()));
+                        var range = Byte.Parse(new String(chars.SkipWhile(c => Char.ToLower(c) != 'd').Skip(1).ToArray()));
+                        answer += new Dice(count, range);
+                    }
+                }
+                else if (chars[0] == '-')
+                {
+                    if (Char.IsNumber(chars[1]))
+                    {
+                        answer += Int64.Parse(new String(chars));
+                    }
+                    else if (chars[1] == '!')
+                    {
+                        var count = -Int64.Parse(new String(chars.Skip(2).TakeWhile(c => Char.ToLower(c) != 'd').ToArray()));
+                        var range = Byte.Parse(new String(chars.SkipWhile(c => Char.ToLower(c) != 'd').Skip(1).ToArray()));
+                        answer += new Dice(count, range);
+                    }
+                }
+                else if (chars[0] == '!')
+                {
+                    var count = Int64.Parse(new String(chars.Skip(1).TakeWhile(c => Char.ToLower(c) != 'd').ToArray()));
+                    var range = Byte.Parse(new String(chars.SkipWhile(c => Char.ToLower(c) != 'd').Skip(1).ToArray()));
+                    answer += new Dice(count, range);
+                }
+                else if (Char.IsNumber(chars[0]))
+                {
+                    answer += Int64.Parse(new String(chars));
+                }
+            }
+            return answer;
+        }
+
+        /// <summary>
+        /// Parseメソッド下でのみ使用される
+        /// Parseするべき文字列を受け取り、単一ダイスと単一定数のリストに分解して返す
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private static List<char[]> _Parse_CutIntoPieces(Char[] str)
+        {
+            List<Char[]> splited = new List<Char[]>();
+            List<Char> temp = new List<Char>();
+            for (Int32 i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case '+':
+                        if (temp.Count == 0) continue;
+                        splited.Add(temp.ToArray());
+                        temp.Clear();
+                        temp.Add('+');
+                        break;
+                    case '-':
+                        if (temp.Count == 0)
+                        {
+                            temp.Add('-');
+                            continue;
+                        }
+                        splited.Add(temp.ToArray());
+                        temp.Clear();
+                        temp.Add('-');
+                        break;
+                    default:
+                        temp.Add(str[i]);
+                        break;
+                }
+            }
+            if (temp.Count != 0) splited.Add(temp.ToArray());
+            return splited;
+        }
+
         #region Constructor
         public Dice() : base(new Dictionary<Byte, Int64>(), 0) { }
         public Dice(Int64[] counts, Byte[] ranges, Int64 constant)
@@ -82,6 +183,28 @@ namespace ChainSystem
         }
         #endregion
 
+
+        #region Cast
+        /// <summary>
+        /// ダイスを振る
+        /// </summary>
+        /// <param name="random">乱数初期化子</param>
+        /// <param name="diceCast">出た目をゼロ以上にするかなどの処理</param>
+        /// <returns>出た目</returns>
+        public Int64 Cast(Random random, DiceCast diceCast)
+        {
+            var answer = Cast(random);
+            switch (diceCast)
+            {
+                case DiceCast.ApproveMinus:
+                    return answer;
+                case DiceCast.NotMinus:
+                    return answer < 0 ? 0 : answer;
+                case DiceCast.OverZero:
+                    return answer < 1 ? 1 : answer;
+            }
+            return answer;
+        }
         /// <summary>
         /// ダイスを振る
         /// 実行するたびに値が変化する
@@ -91,15 +214,25 @@ namespace ChainSystem
         {
             Int64 answer = 0;
             foreach (var t in Item1)
-                for (Int64 i = 0; i < t.Value; i++)
-                    answer += random.Next(t.Key) + 1;
+            {
+                for (Int64 i = 0; i < Math.Abs(t.Value); i++)
+                {
+                    if (t.Value >= 0)
+                        answer += random.Next(t.Key) + 1;
+                    else
+                        answer -= random.Next(t.Key) + 1;
+                }
+            }
             return answer + Item2;
         }
         public Int64 Cast()
         {
             return Cast(new Random());
         }
+        public enum DiceCast : byte { NotMinus = 0, OverZero = 1, ApproveMinus = 2 }
+        #endregion
 
+        #region 最初から用意されているダイス
         /// <summary>
         /// 1d100ダイス
         /// </summary>
@@ -109,9 +242,10 @@ namespace ChainSystem
         /// </summary>
         public static readonly Dice D1D10 = new Dice(1, 10);
         /// <summary>
-        /// 3d6ダイス
+        /// 1d6ダイス
         /// </summary>
-        public static readonly Dice D3D6 = new Dice(3, 6);
+        public static readonly Dice D1D6 = new Dice(1, 6);
+        #endregion
 
         /// <summary>
         /// Keyである面数が一致したKey Value Pair同士をValueを足して統合する
@@ -121,7 +255,7 @@ namespace ChainSystem
         /// <param name="obj0">1つめのDictionary</param>
         /// <param name="obj1">2つめのDictionary</param>
         /// <returns>Mergeされて新たに作られたDictionary</returns>
-        private static Dictionary<Byte, Int64> MergeDictionary(Dictionary<Byte, Int64> obj0, Dictionary<Byte, Int64> obj1)
+        private static Dictionary<Byte, Int64> _Operator_MergeDictionary(Dictionary<Byte, Int64> obj0, Dictionary<Byte, Int64> obj1)
         {
             var answer = new Dictionary<Byte, Int64>(obj0);
             foreach (var keyval in obj1)
@@ -143,7 +277,7 @@ namespace ChainSystem
         #region Operator
         public static Dice operator +(Dice obj0, Dice obj1)
         {
-            return new Dice(MergeDictionary(obj0.Item1, obj1.Item1), obj0.Constant + obj1.Constant);
+            return new Dice(_Operator_MergeDictionary(obj0.Item1, obj1.Item1), obj0.Constant + obj1.Constant);
         }
         public static Dice operator +(Dice obj, Int64 constant)
         {
@@ -159,7 +293,7 @@ namespace ChainSystem
         }
         public static Dice operator -(Dice obj0, Dice obj1)
         {
-            return new Dice(MergeDictionary(obj0.Item1, obj1.Item1.ToDictionary(keyval => keyval.Key, keyval => -(keyval.Value))), obj0.Constant - obj1.Constant);
+            return new Dice(_Operator_MergeDictionary(obj0.Item1, obj1.Item1.ToDictionary(keyval => keyval.Key, keyval => -(keyval.Value))), obj0.Constant - obj1.Constant);
         }
         public static Dice operator -(Dice obj, Int64 constant)
         {
@@ -232,11 +366,8 @@ namespace ChainSystem
                         }
                         else
                         {
-                            var val = Math.Abs(keyval.Value);
-                            for (Int64 i = 0; i < val / 10; i++)
-                            {
-                                answer.Append("-!10d").Append(keyval.Key);
-                            }
+                            var val = Math.Abs(keyval.Value == Int64.MinValue ? -1 : keyval.Value);
+                            answer.Insert(answer.Length - 1, "-!10d" + keyval.Key.ToString(), (Int32)(val / 10));
                             if (val % 10 != 0)
                             {
                                 answer.Append("-!").Append(val % 10).Append('d').Append(keyval.Key);
@@ -252,14 +383,16 @@ namespace ChainSystem
                         var val = Math.Abs(keyval.Value);
                         if (keyval.Value >= 0)
                         {
-                            for (Int64 i = 0; i < val / 10; i++) {
+                            for (Int64 i = 0; i < val / 10; i++)
+                            {
                                 answer.Append("+!10d").Append(keyval.Key);
                             }
-                            if (val % 10 != 0) {
+                            if (val % 10 != 0)
+                            {
                                 answer.Append("+!").Append(val % 10).Append('d').Append(keyval.Key);
                             }
                         }
-                        else 
+                        else
                         {
                             for (Int64 i = 0; i < val / 10; i++)
                             {
