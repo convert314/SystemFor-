ce0f97c32ad969c313e8bc38a0ecce79269b9330
convert314 <convert314@gmail.com>
2015-06-18 12:51:38 +0900

    追記漏れ。

---
+++ ChainSystem/HomoSapiens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoC;
+
+namespace CoC.ChainSystem
+{
+    public class HomoSapiens : IHomoSapiens
+    {
+    }
+}
