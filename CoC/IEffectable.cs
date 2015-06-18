a80ebe75aed55d0bac23e7f21872693c6a077b4c
convert314 <convert314@gmail.com>
2015-06-19 02:02:11 +0900

    著作権に関連する安全策

--- CoC/IEffectable.cs
+++ CoC/IEffectable.cs
@@ -6,12 +6,41 @@ using System.Threading.Tasks;
 
 namespace CoC
 {
-    public interface IEffectable : IGameObject, IObservable<News>
+    public interface IEffectable : IObservable<News>
     {
+        /// <summary>
+        /// 所有者のIGameObjectです。
+        /// 全てのIEffectableには効果の所有者があるべきです。
+        /// </summary>
         IGameObject Owner { get; }
+        /// <summary>
+        /// Newsを受け取って実行可能かどうか判断します。
+        /// </summary>
+        /// <param name="news">Coc.News型の情報です。この情報のみに基づいて実行可能かどうか判断します。</param>
+        /// <returns>実行可能かどうかを表すSystem.Boolean型</returns>
         Boolean IsExecutable(News news);
+        /// <summary>
+        /// Newsをもとに対象のIGameObjectに操作を施します。
+        /// </summary>
+        /// <param name="news">Coc.News型の情報です。操作するための大事な情報です。</param>
+        /// <param name="obj">操作対象のCoC.IGameObjectです。</param>
         void Execute(News news, IGameObject obj);
-        IEffectable[] PartialEffects { get; }
-        Boolean IsPartialExecutable(News news, out Int64 index);
+        /// <summary>
+        /// 効果によっては複数のIEffectableから構成されています。
+        /// その複数のIEffectableの配列を表すプロパティです。
+        /// </summary>
+        IEffectable[] PartialEffectables { get; }
+        /// <summary>
+        /// PartialIEffectablesの内実行可能なものがあるかどうか調べます。
+        /// 実行可能なIEffectableが存在する場合、そのインデックスを返します。
+        /// </summary>
+        /// <param name="news"></param>
+        /// <param name="index">
+        /// out Int64[]
+        /// 実行可能なインデックスの配列です。
+        /// 存在しない場合要素数0の配列を返します。
+        /// </param>
+        /// <returns>実行可能なIEffectableが存在するかどうかを表すSystem.Boolean値</returns>
+        Boolean IsPartialExecutable(News news, out Int64[] index);
     }
 }
